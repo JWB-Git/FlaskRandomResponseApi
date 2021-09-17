2ff9c1244a85bf84bff2b54b906ac060b0324eb6
jack.burgess <jack.burgess@bjss.com>
2021-09-17 12:29:59 +0100

    Fixed cross origin and minor response changes

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import random
-import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-with open('responses.json') as responseFile:
-    responses = json.load(responseFile)
+responses = [
+    "How much would a wood chuck chuck if a wood chuck could chuck wood",
+    "Your team will win this weekend!",
+    "BJSS will rule the world!",
+    "This is certainly a well trained model",
+    "O Romeo, Romeo, wherefore art thou Romeo?",
+    "Pub? Pub!",
+    "There are only 10 types of people in the world – those who understand binary, and those who don’t.",
+    "Today shall be sunny, or rainy idk",
+    "LOL ROFL",
+    "Go get a Greggs, you deserve it"
+]
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET', 'POST'])
+@cross_origin()
 def index():
     rand_response = random.choice(responses)
 
